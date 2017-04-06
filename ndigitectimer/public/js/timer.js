0c9b96b8c704fd7527cb99d8fe6d470a54ef5847
John Harutyunyan <john.harutyunyan1995@gmail.com>
2017-04-06 17:47:03 +0400

    Added as https ndigtec link

--- ndigitectimer/public/js/timer.js
+++ ndigitectimer/public/js/timer.js
@@ -15,7 +15,7 @@ TrelloPowerUp.initialize({
         // return a promise that resolves to the object with
         // a property 'authorized' being true/false
         // you can also return the object synchronously if you know the answer synchronously
-        return new TrelloPowerUp.Promise((resolve) >= resolve({ authorized: true }));
+        return new TrelloPowerUp.Promise((resolve) => resolve({ authorized: true }));
     },
 
     'card-buttons': function () {
